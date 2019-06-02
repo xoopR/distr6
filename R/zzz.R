21543cf811a3f280544683ecfb6656641da0e909
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-06-02 14:26:43 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
-  R62S3::R62S3(Distribution, assignEnvir = environment())
-  R62S3::R62S3(ParameterSet, assignEnvir = environment())
+  R62S3::R62S3(Distribution, assignEnvir = -1)
+  R62S3::R62S3(ParameterSet, assignEnvir = -1)
 
   y = sapply(ls(name=".GlobalEnv"),function(x){
     if(inherits(get(x),"R6ClassGenerator")){
@@ -12,5 +12,5 @@
       return(FALSE)
   })
   y = (y[y!="FALSE"])
-  lapply(y,R62S3::R62S3, assignEnvir = environment())
+  lapply(y,R62S3::R62S3, assignEnvir = -1)
 }
