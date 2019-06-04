62868ec0580bd06c931ea726facc530dec09215f
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-06-04 09:45:51 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 .onAttach <- function(libname, pkgname){
   R62S3::R62S3(Distribution, assignEnvir = as.environment("package:distr6"))
   R62S3::R62S3(ParameterSet, assignEnvir = as.environment("package:distr6"))
+  R62S3::R62S3(ExoticStatistics, assignEnvir = as.environment("package:distr6"))
+  R62S3::R62S3(CoreStatistics, assignEnvir = as.environment("package:distr6"))
 
   y = sapply(ls(name=".GlobalEnv"),function(x){
     if(inherits(get(x),"R6ClassGenerator")){
