f0decb6feeede9578a906a285a4d60ed6b3d08ae
Xiaowen Huang <46967322+w090613@users.noreply.github.com>
2019-06-14 14:31:46 +0100

    Update Distribution_Weibull.R

--- R/Distribution_Weibull.R
+++ R/Distribution_Weibull.R
@@ -1,12 +1,10 @@
-library(R6)
-library(distr6)
 Weibull <- R6::R6Class("Weibull", inherit = Distribution, lock_objects = F)
 Weibull$set("public","name","Weibull")
 Weibull$set("public","short_name","Weibull")
 Weibull$set("public","traits",list(type = PosReals$new(zero=T),
                                    valueSupport = "continuous",
                                    variateForm = "univariate"))
-
+Weibull$set("public","description","Weibull Probability Distribution.")
 
 Weibull$set("public","mean",function(){
   self$getParameterValue("scale")*digamma(1+1/self$getParameterValue("shape"))
@@ -26,7 +24,8 @@ Weibull$set("public","skewness",function() {
 })
 
 Weibull$set("public","kurtosis",function(excess = TRUE){
-  kur<-(l^4*digamma(1+4/k)-4*self$getParameterValue("skewness")*sigma^3*mu-6*sigma^2*mu^2-mu^4)/sigma^4 - 3
+  skew<-(digamma(1+3/k)*l^3-3*mu*sigma^2-mu^3)/sigma^3
+  kur<-(l^4*digamma(1+4/k)-4*skew*sigma^3*mu-6*sigma^2*mu^2-mu^4)/sigma^4 - 3
   if(excess)
     return(kur)
   else
@@ -38,41 +37,28 @@ Weibull$set("public","entropy",function(base = 2){
 })
 
 
-Weibull$set("public","survival",function(x1, log.p = FALSE){
-  self$cdf(x1, lower.tail = FALSE, log.p)
-})
-
-Weibull$set("public","hazard",function(x1){
-  self$pdf(x1)/self$survival(x1)
-})
-
-Weibull$set("public","cumHazard",function(x1){
-  -self$cdf(x1, log.p = TRUE)
-})
 
 Weibull$set("public","mode",function(){
-  return(max(self$getParameterValue("df")-2,0))
+  if(k>1){
+  return (l*((k-1)/k)^(1/k))
+  }
+  else{
+  return(0)
+  }
 })
 
-Weibull$set("private",".parameters", NULL)
+Weibull$set("private",".getRefParams", function(paramlst){
+  lst = list()
+  if(!is.null(paramlst$shape)) lst = c(lst, list(shape=shape))
+  if(!is.null(paramlst$scale)) lst = c(lst, list(scale=scale))
+  return(lst)
+})
 
 Weibull$set("public","initialize",function(shape = 1, scale= 1, decorators = NULL,...){
   
-  
-  private$.parameters <- ParameterSet$new(id = list("shape","scale"),
-                                          value = list(1, 1),
-                                          lower = list(0, 0 ),
-                                          upper = list(Inf, Inf),
-                                          class = list("numeric","numeric"),
-                                          settable = list(TRUE, TRUE),
-                                          updateFunc = list(NA, NA),
-                                          description = list("Shape",
-                                                             "Scale"))
-  
-  
-  self$setParameterValue(list(shape=shape,scale=scale))
-  
-  
+  private$.parameters <- getParameterSet(self, shape, scale, verbose)
+  self$setParameterValue(list(shape=shape,rate = rate ))
+   
   pdf <- function(x1) dWeibull(x1, self$getParameterValue("shape"), self$getParameterValue("scale"))
   cdf <- function(x1) pWeibull(x1, self$getParameterValue("shape"), self$getParameterValue("scale"))
   quantile <- function(p) qWeibull(p, self$getParameterValue("shape"), self$getParameterValue("scale"))
