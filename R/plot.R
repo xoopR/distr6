0d7418033a27fd976c99cf1ffc33763abfe3c505
shliu99 <45162294+shliu99@users.noreply.github.com>
2019-06-26 23:26:38 +0100

    Update plot.R

--- R/plot.R
+++ R/plot.R
@@ -14,7 +14,7 @@ plot.SDistribution <- function(x, fun, nPoints = 3000, coverage = 0.99,
   #######################################################################
   #
   #   To set up quantitis required to produce pdf/cdf/quantile/survival/
-  #   hazard/cumulative hazard plots. Arguements:
+  #   hazard/cumulative hazard plots. Arguments:
   #
   #   x         A SDistribution class object
   #   fun       List of plots to be produced
@@ -32,7 +32,7 @@ plot.SDistribution <- function(x, fun, nPoints = 3000, coverage = 0.99,
   #   add       A logical factor indicated whether to have new plots
   #             superimposed over an existing plot.
   #   layout.row A logical factor that only works if iterative == FALSE.
-  #             When iterative == TRUE, this arguement will be ignored.
+  #             When iterative == TRUE, this argument will be ignored.
   #             If TRUE, the layout of figures in the plot window will be
   #             decided by default (1*3 for three plots, 2*2 for 4 plots
   #             and 2*3 for 5/6 plots). Once plots are produced, the
@@ -51,7 +51,7 @@ plot.SDistribution <- function(x, fun, nPoints = 3000, coverage = 0.99,
   #             equal 0.5. (e.g. coverage = 0.9 will plot points from 0.05
   #             quantile to 0.95 quantile).
   #   ...       All graphical parameters in current R plot function can 
-  #             be added as additional arguements.
+  #             be added as additional arguments.
   #
   #######################################################################
   
