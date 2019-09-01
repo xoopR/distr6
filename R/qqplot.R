92d70fde72022306eabc7da291116909ee6cdf6b
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-09-01 10:10:16 +0100

    Update qqplot.R

--- R/qqplot.R
+++ R/qqplot.R
@@ -11,12 +11,12 @@
 #   y             A SDistribution class object.
 #   nPoints       Number of evaluation points to be generated for each
 #                 distribution, the default is set to be 3,000.
-#   pos           A numerical parameter between 0 and 1, indicating the 
+#   pos           A numerical parameter between 0 and 1, indicating the
 #                 value of plotting position constant, the default is
 #                 set to be 0.5.
-#   plot          A logical factor indicated whether to produce the 
-#                 quantile plot. 
-#                 If false, a list containing graphical information 
+#   plot          A logical factor indicated whether to produce the
+#                 quantile plot.
+#                 If false, a list containing graphical information
 #                 (x0, y0 and other arguments) will be returned.
 #                 The default is set to be TRUE.
 #   withIdLine    A logical factor indicated whether to produce plot for
@@ -24,7 +24,7 @@
 #                 The default is set to be TRUE.
 #   withConf      A logical factor indicated whether to produce plots for
 #                 confidence lines, only works if plot = TRUE.
-#                 The default is set to be TRUE. 
+#                 The default is set to be TRUE.
 #   withConf.pw   A logical factor indicated whether to produce plots for
 #                 pointwise confidence lines, only works if withConf = TRUE.
 #                 The default is set to be TRUE.
@@ -33,7 +33,7 @@
 #                 The default is set to be TRUE.
 #   xlab          x-lable.
 #   ylab          y-lable.
-#   ...           All graphical parameters in current R plot function can 
+#   ...           All graphical parameters in current R plot function can
 #                 be added as additional arguments.
 #
 #######################################################################
@@ -41,110 +41,94 @@
 library(distr6)
 #library(EnvStats)
 
-qqplot.Distribution <- function(x, y, nPoints = 3000, pos = 0.5, 
-                                withIdLine = TRUE, withConf = TRUE, 
-                                withConf.pw  = withConf, withConf.sim = withConf, 
-                                plot = TRUE, xlab = deparse(substitute(x)), 
+qqplot.Distribution <- function(x, y, nPoints = 3000, pos = 0.5,
+                                withIdLine = TRUE, withConf = TRUE,
+                                withConf.pw  = withConf, withConf.sim = withConf,
+                                plot = TRUE, xlab = deparse(substitute(x)),
                                 ylab = deparse(substitute(y)), ...){
-  
-  
+
+
   # random number generator: sample quantiles from 0 to 1 (cdf)
   points <- seq(0,1,length.out = nPoints)
-  
-  
-  ##If y is a list of R6 Distributions##
-  if(testDistributionList(list(x,y))){
-    
-    # calculate quantile.X, quantile.Y
-    quantile.X <- x$quantile(points)
-    quantile.Y <- y$quantile(points)
-    
-  }
-  ##If y is emperical distribution or random sample points##
-  else{
-    
-    if(testDistributionList(list(x))){
-      quantile.X <- x$quantile(points)
-    }else{
-      quantile.X <- sort(x)
+
+  if(is.numeric(x))
+    x <- Empirical$new(x)
+  if(is.numeric(y))
+    y <- Empirical$new(y)
+
+  assertDistributionList(list(x, y), "x and y should both be distributions")
+
+  quantile.X <- x$quantile(points)
+  quantile.Y <- y$quantile(points)
+
+  nx <- length(quantile.X)
+  ny <- length(quantile.Y)
+
+  if (nx != ny) {
+    ppoints.x <- ppoints(nx, a = pos)
+    ppoints.y <- ppoints(ny, a = pos)
+    if (nx > ny) {
+      quantile.X <- approx(ppoints.x, quantile.X, xout = ppoints.y,
+                           rule = 2)$y
     }
-    
-    if(testDistributionList(list(y))){
-      quantile.Y <- y$quantile(points)
-    }else{
-      quantile.Y <- sort(y)
-    }
-    
-    nx <- length(quantile.X)
-    ny <- length(quantile.Y)
-    
-    if (nx != ny) {
-      ppoints.x <- ppoints(nx, a = pos)
-      ppoints.y <- ppoints(ny, a = pos)
-      if (nx > ny) {
-        quantile.X <- approx(ppoints.x, quantile.X, xout = ppoints.y, 
-                             rule = 2)$y
-      }
-      else {
-        quantile.Y <- approx(ppoints.y, quantile.Y, xout = ppoints.x, 
-                             rule = 2)$y
-      }
+    else {
+      quantile.Y <- approx(ppoints.y, quantile.Y, xout = ppoints.x,
+                           rule = 2)$y
     }
   }
-  
-  
+
   #Now plot!
-  
+
   ##Quantile vs Quantile##
   if(plot == TRUE){
     plot(x = quantile.X, y = quantile.Y, type = "p", col = "black",
          xlab = deparse(substitute(x)), ylab = deparse(substitute(y)),...)
-    
+
     ##Identity Line##
     if(withIdLine == TRUE){
       abline(a = 0, b = 1, col = "blue", lty = 2, lwd = 2)
     }
-    
+
     ##Confidence Lines##
     #if(withConf == TRUE){
-      
-      #x0 <- quantile.X 
+
+      #x0 <- quantile.X
       #y0 <- quantile.Y
-      
+
       #quantile.df <- data.frame(x0, y0)
-      
-      #quantile.fit <- lm(y0 ~ x0, data = quantile.df) 
-      
+
+      #quantile.fit <- lm(y0 ~ x0, data = quantile.df)
+
       #new.x0 <- seq(min(x0), max(x0), length=100)  #how to generate newdata?
-      
-      #predict.quantile <- predict(quantile.fit, 
-                                  #newdata = data.frame(x0 = new.x0), se.fit = TRUE) 
-      
+
+      #predict.quantile <- predict(quantile.fit,
+                                  #newdata = data.frame(x0 = new.x0), se.fit = TRUE)
+
       ##Pointwise Confidence Interval##
       #if(withConf.pw == TRUE){
-        #ci.pw <- pointwise(predict.quantile, coverage=0.95) 
-        
-        #lines(new.x0, ci.pw$lower, lty=3, col = "orange", lwd = 2) 
-        #lines(new.x0, ci.pw$upper, lty=3, col = "orange", lwd = 2) 
+        #ci.pw <- pointwise(predict.quantile, coverage=0.95)
+
+        #lines(new.x0, ci.pw$lower, lty=3, col = "orange", lwd = 2)
+        #lines(new.x0, ci.pw$upper, lty=3, col = "orange", lwd = 2)
       #}
-      
+
       ##Simultaneous Confidence Interval##
       #if(withConf.sim==TRUE){
-        #ci.sim <- pointwise(predict.quantile, coverage=0.95, 
-                            #simultaneous=TRUE) 
-        
-        #lines(new.x0, ci.sim$lower, lty=4, col = "red", lwd = 2) 
+        #ci.sim <- pointwise(predict.quantile, coverage=0.95,
+                            #simultaneous=TRUE)
+
+        #lines(new.x0, ci.sim$lower, lty=4, col = "red", lwd = 2)
         #lines(new.x0, ci.sim$upper, lty=4, col = "red", lwd = 2)
       #}
-      
+
       ##Legend
       #legend("topleft", legend = c("Pintwise CI", "Simultaneous CI"), col = c("orange", "red"),
              #lty = c(3,4), cex = 0.8)
     #}
-    
+
   }else{
     returnList = list(x0 = quantile.X, y0 = quantile.Y)
-    
+
     return(returnList)
   }
 }
@@ -157,7 +141,7 @@ qqplot.Distribution <- function(x, y, nPoints = 3000, pos = 0.5,
 qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15))
 
 qqplot.Distribution(Empirical$new(rnorm(n=3000,mean=15,sd=sqrt(30))), Empirical$new(rchisq(n=3000,df=15)))
-qqplot.Distribution(rnorm(n=3000,mean=15,sd=sqrt(30)), rchisq(n=3000,df=15), 
+qqplot.Distribution(rnorm(n=3000,mean=15,sd=sqrt(30)), rchisq(n=3000,df=15),
                     pos = 0.9)
 
 qqplot.Distribution(rnorm(n=20,mean=15,sd=sqrt(30)), c(3,6,8,2,0,6,2,7,9,4,3,7,4,5,11,1,10,6,6,3))
@@ -165,13 +149,13 @@ qqplot.Distribution(rnorm(n=20,mean=15,sd=sqrt(30)), c(3,6,8,2,0,6,2,7,9,4,3,7,4
 qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), Empirical$new(rchisq(n=3000,df=15)))
 qqplot.Distribution(rnorm(n=3000,mean=15,sd=sqrt(30)), ChiSquared$new(df = 15))
 
-qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15), 
+qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15),
                     withIdLine = FALSE)
 
-#qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15), 
+#qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15),
 #                    withConf = FALSE)
-#qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15), 
+#qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15),
 #                    withConf.pw = FALSE)
-#qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15), 
+#qqplot.Distribution(Normal$new(mean = 15, sd = sqrt(30)), ChiSquared$new(df = 15),
 #                    withConf.sim = FALSE)
 
