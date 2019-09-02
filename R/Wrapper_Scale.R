d4452607d934fa1bee2784105e002b11ae720099
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-09-02 07:55:07 +0100

    scale docs

--- R/Wrapper_Scale.R
+++ R/Wrapper_Scale.R
@@ -1,124 +1,110 @@
-#' #' @title Distribution Scale Wrapper
-#' #'
-#' #' @description Scales a distribution to a given mean and standard deviation. By default the distribution
-#' #' is centered (\code{mean} = 0) with unit variance (\code{sd} = 1)
-#' #'
-#' #' @details STILL IN DEVELOPMENT. Results are likely sub-optimal.
-#' #
-#' #' @name Scale
-#' #'
-#' #' @section Constructor Arguments:
-#' #' \tabular{lll}{
-#' #' \strong{Argument} \tab \strong{Type} \tab \strong{Details} \cr
-#' #' \code{dist} \tab distribution \tab Distribution to scale. \cr
-#' #' \code{mean} \tab numeric \tab Desired mean after distribution shift. \cr
-#' #' \code{sd} \tab numeric \tab Desired standard deviation after distribution scale.
-#' #' }
-#' #'
-#' #' @section Getters:
-#' #' \tabular{lll}{
-#' #' \strong{Method} \tab \strong{Return Type} \tab \strong{Details} \cr
-#' #' \code{getScaleMean()} \tab numeric \tab Return mean of scaled distribution. \cr
-#' #' \code{getScaleSd()} \tab numeric \tab Return standard deviation of scaled distribution. \cr
-#' #' }
-#' #'
-#' #'@section Setters:
-#' #' \tabular{lll}{
-#' #' \strong{Method} \tab \strong{Input Type} \tab \strong{Details} \cr
-#' #' \code{setScaleMean(mean)} \tab numeric \tab Set mean to scale distribution to. \cr
-#' #' \code{setScaleSd(sd)} \tab numeric \tab Set standard deviation to scale distribution to. \cr
-#' #' }
-#' #'
-#' #' @examples
-#' #' \dontrun{
-#' #' Scale$new(Binomial$new())
-#' #' }
-#' #'
-#' #' @seealso See \code{\link{DistributionWrapper}} for inherited wrapper methods and see \code{\link{Distribution}}
-#' #' for a full list of inherited distribution methods.
+#' @title Distribution Scale Wrapper
 #'
+#' @description Scales a distribution to a given mean and standard deviation. By default the distribution
+#' is centered (\code{mean} = 0) with unit variance (\code{sd} = 1)
+#'
+#' @details STILL IN DEVELOPMENT. Results are likely sub-optimal.
+#
+#' @name Scale
+#'
+#' @section Constructor Arguments:
+#' \tabular{lll}{
+#' \strong{Argument} \tab \strong{Type} \tab \strong{Details} \cr
+#' \code{dist} \tab distribution \tab Distribution to scale. \cr
+#' \code{mean} \tab numeric \tab Desired mean after distribution shift. \cr
+#' \code{sd} \tab numeric \tab Desired standard deviation after distribution scale.
+#' }
+#'
+#' @section Getters:
+#' \tabular{lll}{
+#' \strong{Method} \tab \strong{Return Type} \tab \strong{Details} \cr
+#' \code{getscaledmean()} \tab numeric \tab Return mean of scaled distribution. \cr
+#' \code{getscaledsd()} \tab numeric \tab Return standard deviation of scaled distribution. \cr
+#' }
+#'
+#'@section Setters:
+#' \tabular{lll}{
+#' \strong{Method} \tab \strong{Input Type} \tab \strong{Details} \cr
+#' \code{setscaledmean(mean)} \tab numeric \tab Set mean to scale distribution to. \cr
+#' \code{setscaledsd(sd)} \tab numeric \tab Set standard deviation to scale distribution to. \cr
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' Scale$new(Binomial$new())
+#' }
+#'
+#' @seealso See \code{\link{DistributionWrapper}} for inherited wrapper methods and see \code{\link{Distribution}}
+#' for a full list of inherited distribution methods.
+#' @export
+NULL
 Scale <- R6::R6Class("Scale", inherit = DistributionWrapper, lock_objects = FALSE)
-Scale$set("public","initialize",function(dist,mean=NULL,sd=NULL,var=NULL,verbose=TRUE,...){
+Scale$set("public","initialize",function(dist, mean = 0, sd = 1, var = NULL,...){
      assertDistribution(dist)
-     dist = dist$clone()
+     dist = dist$clone(deep = TRUE)
 
      name = dist$name
      short_name = dist$short_name
 
      distlist = list(dist)
      names(distlist) = short_name
-     
-     if(is.null(var)){
-        if(is.null(sd)){
-           if(verbose){ message("the scale sd is set to be 1") }
-           sd=1
-           var = 1
-        }else{
-           var = sd^2
-     }
-        
-     }else{
-        if(is.null(sd))
-           sd = sqrt(var)
-        else
-           if(sd^2!=var)
-              if(verbose){ message("the scale sd^2 must be equal to var") }
-     }
-     
-     if(is.null(mean)){
-        if(verbose){message("the scale mean is set to be 0")}
-        mean=0
-     }
 
-     private$.outerParameters <- ParameterSet$new(id = list("ScaleMean","ScaleSd","ScaleVar"), value = list(mean,sd,var),
-                                                  support = list(Reals$new(),PosReals$new(),PosReals$new()), settable = list(FALSE,FALSE,FALSE),
+     if(!is.null(var))
+       sd = sqrt(var)
+     else
+       var = sd^2
+
+     private$.outerParameters <- ParameterSet$new(id = list("scaledmean","scaledsd","scaledvar"),
+                                                  value = list(mean, sd, var),
+                                                  support = list(Reals$new(), PosReals$new(), PosReals$new()),
+                                                  settable = list(FALSE, FALSE, FALSE),
+                                                  updateFunc = list(NA, NA,
+                                                                    function(self) self$getParameterValue("scaledsd")^2),
                                                   description = list("Mean of the output scaled distribution.",
                                                                      "Standard deviation of the output scaled distribution.",
-                                                                     "Vairance of the output scaled distribution"))
-     self$setParameterValue(ScaleSd=sd)
-     self$setParameterValue(ScaleMean=mean)
-     self$setParameterValue(ScaleVar=var)
-     
-     if(!is.null(dist$pdf(1))){
+                                                                     "Variance of the output scaled distribution"))
+
+     if(dist$.__enclos_env__$private$.isPdf){
         pdf <- function(x1){}
         body(pdf) <- substitute({
-           scaleTrafo <- self$getParameterValue("ScaleSd")/self$wrappedModels()[[1]]$stdev()
-           self$wrappedModels()[[1]]$pdf(-self$getParameterValue("ScaleMean")+(x1+self$wrappedModels()[[1]]$mean())*scaleTrafo)
+           scaleTrafo <- self$getParameterValue("scaledsd")/self$wrappedModels()[[1]]$stdev()
+           self$wrappedModels()[[1]]$pdf(-self$getParameterValue("scaledmean")+(x1+self$wrappedModels()[[1]]$mean())*scaleTrafo)
         }, list(name = short_name))
-     } else{pdf <- NULL}
-     
-     if(!is.null(dist$cdf(1))){
-        
+     } else
+       pdf <- NULL
+
+     if(dist$.__enclos_env__$private$.isCdf){
         cdf <- function(x1){}
         body(cdf) <- substitute({
-           scaleTrafo <- self$getParameterValue("ScaleSd")/self$wrappedModels(name)$stdev()
-           self$wrappedModels(name)$cdf(-self$getParameterValue("ScaleMean")+(x1+self$wrappedModels(name)$mean())*scaleTrafo)
+           scaleTrafo <- self$getParameterValue("scaledsd")/self$wrappedModels(name)$stdev()
+           self$wrappedModels(name)$cdf(-self$getParameterValue("scaledmean")+(x1+self$wrappedModels(name)$mean())*scaleTrafo)
         }, list(name = short_name))
-     } else{cdf <- NULL}
-     
+     } else
+       cdf <- NULL
+
      name = paste("Scaled",name)
      short_name = paste0("Scaled",short_name)
-  
-     type = Reals$new()
-  
+
+     type = dist$type()
+
      super$initialize(distlist = distlist, pdf = pdf, cdf = cdf, name = name,
                       short_name = short_name, type = type,...)
    }) # IN PROGRESS
- 
+
 
 Scale$set("public","setParameterValue",function(..., lst = NULL, error = "warn"){
    if(is.null(lst))
       lst <- list(...)
-   if("ScaleSd"%in%names(lst) & "ScaleVar"%in%names(lst)){
-      checkmate::assert(lst[["ScaleSd"]]^2 == lst[["ScaleVar"]])
-   }else if("ScaleSd"%in%names(lst)){
-      lst[["ScaleVar"]] = lst[["ScaleSd"]]^2
-   }else if("ScaleVar"%in%names(lst)){
-      lst[["ScaleSd"]] = sqrt(lst[["ScaleVar"]])
+   if("scaledsd"%in%names(lst) & "scaledvar"%in%names(lst)){
+      checkmate::assert(lst[["scaledsd"]]^2 == lst[["scaledvar"]])
+   }else if("scaledsd"%in%names(lst)){
+      lst[["scaledvar"]] = lst[["scaledsd"]]^2
+   }else if("scaledvar"%in%names(lst)){
+      lst[["scaledsd"]] = sqrt(lst[["scaledvar"]])
    }
    super$setParameterValue(lst=lst,error=error)
 })
-   
 
-   
-   
+
+
+
