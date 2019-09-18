4c9a24cd8051fc629e15f51e75bbe737c8566071
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-09-18 11:10:23 +0100

    Update Wrapper_AffineTransform.R

--- R/Wrapper_AffineTransform.R
+++ R/Wrapper_AffineTransform.R
@@ -1,15 +1,15 @@
-## Note the purpose of introducing Shift is to test how useful the wrapper is for shifting X to Y=aX+b
-Shift <- R6::R6Class("Shift", inherit = DistributionWrapper, lock_objects = FALSE)
-Shift$set("public","initialize",function(dist, a= 1,b=0,verbose=TRUE,...){
+## Note the purpose of introducing AffineTransform is to test how useful the wrapper is for AffineTransforming X to Y=aX+b
+AffineTransform <- R6::R6Class("AffineTransform", inherit = DistributionWrapper, lock_objects = FALSE)
+AffineTransform$set("public","initialize",function(dist, a= 1,b=0,verbose=TRUE,...){
     assertDistribution(dist)
     dist = dist$clone(deep = TRUE)
-    
+
     name = dist$name
     short_name = dist$short_name
-    
+
     distlist = list(dist)
     names(distlist) = short_name
-    
+
     if(is.null(a)){
         if(verbose == TRUE){
             a = 1 }else{
@@ -22,39 +22,39 @@ Shift$set("public","initialize",function(dist, a= 1,b=0,verbose=TRUE,...){
             message("b is set to be 0")
             b = 0 }
     }
-        
+
     private$.outerParameters <- ParameterSet$new(id = list("a","b"),
                                                  value = list(a, b),
                                                  support = list(Reals$new(),Reals$new()),
                                                  settable = list(FALSE, FALSE),
                                                  updateFunc = list(NA, NA),
                                                  description = list("a","b"))
-    
+
     if(dist$.__enclos_env__$private$.isPdf){
         pdf <- function(x1){}
         body(pdf) <- substitute({
-            self$wrappedModels()[[1]]$pdf(a*x1+b)
+            self$wrappedModels()[[1]]$pdf((x1 - b)/a)
         }, list(name = short_name))
     } else
         pdf <- NULL
-    
+
     if(dist$.__enclos_env__$private$.isCdf){
         cdf <- function(x1){}
         body(cdf) <- substitute({
-            self$wrappedModels()[[1]]$cdf(a*x1+b)
+            self$wrappedModels()[[1]]$cdf((x1 - b)/a)
         }, list(name = short_name))
     } else
         cdf <- NULL
-    
-    name = paste("Shifted",name)
-    short_name = paste0("Shifted",short_name)
-    
+
+    name = paste("AffineTransformed",name)
+    short_name = paste0("AffineTransformed",short_name)
+
     super$initialize(distlist = distlist, pdf = pdf, cdf = cdf, name = name,
-                     short_name = short_name, type = Reals$new(),
-                     support = Reals$new(),...)
+                     short_name = short_name, type = dist$type(),
+                     support = dist$type(),...)
 }) # IN PROGRESS
 
-Shift$set("public","setParameterValue",function(..., lst = NULL, error = "warn"){
+AffineTransform$set("public","setParameterValue",function(..., lst = NULL, error = "warn"){
     if(is.null(lst))
         lst <- list(...)
     super$setParameterValue(lst=lst,error=error)
