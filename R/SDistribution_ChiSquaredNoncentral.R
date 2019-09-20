13beb0d7b6b939a23194e2a964ce2417937952fd
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-09-20 11:53:20 +0100

    updated docs

--- R/SDistribution_ChiSquaredNoncentral.R
+++ R/SDistribution_ChiSquaredNoncentral.R
@@ -10,7 +10,7 @@
 #' @templateVar uses to model the sum of independent squared Normal distributions and for confidence intervals
 #' @templateVar params degrees of freedom, \eqn{\nu}, and location, \eqn{\lambda},
 #' @templateVar pdfpmf pdf
-#' @templateVar pdfpmfeq \deqn{f(x) = exp(-\lambda/2) \sum_{r=0}^\infty ((\lambda/2)^r/r!) dchisq(x, \nu + 2r)}
+#' @templateVar pdfpmfeq \deqn{f(x) = exp(-\lambda/2) \sum_{r=0}^\infty ((\lambda/2)^r/r!) (x^{(\nu+2\lambda)/2-1} exp(-x/2))/(2^{(\nu+2\lambda)/2}\Gamma((\nu+2\lambda)/2))}
 #' @templateVar paramsupport \eqn{\nu \geq 0}, \eqn{\lambda \geq 0}
 #' @templateVar distsupport the Positive Reals
 #' @templateVar constructor df = 1, location = 0
