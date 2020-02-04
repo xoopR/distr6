b163e96166201f750955f13f14643444d6d8812f
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-02-04 11:12:37 +0000

    deprecated listSpecialSets

--- R/listSpecialSets.R
+++ R/listSpecialSets.R
@@ -1,3 +1,4 @@
+#' @name listSpecialSets-Deprecated
 #' @title Lists Implemented R6 Special Sets
 #' @description Lists special sets that can be used in SetInterval.
 #' @param simplify logical. If FALSE (default) returns data.table of set name and symbol, otherwise set names as characters.
@@ -6,16 +7,13 @@
 #' @examples
 #' listSpecialSets()
 #' listSpecialSets(TRUE)
+#' @keywords internal
+NULL
+
+#' @name listSpecialSets
+#' @rdname distr6-deprecated
 #' @export
-listSpecialSets <- function(simplify = FALSE){
-  y = c("Empty","Naturals","PosNaturals","Integers","PosIntegers","NegIntegers","Rationals",
-        "PosRationals","NegRationals","Reals","PosReals","NegReals","ExtendedReals",
-        "Complex")
-  if(simplify)
-    return(as.character(y))
-  else{
-    return(data.table::data.table(ClassName = y, Symbol = unname(sapply(y, setSymbol)),
-                           Infimum = c("NULL","0","0/1","-Inf","0/1","-Inf","-Inf","0/1","-Inf","-Inf","0/1",rep("-Inf",3)),
-                           Supremum = c("NULL",rep("Inf",4),"-1/0","Inf","Inf","-1/0","Inf","Inf","-1/0","Inf","Inf")))
-  }
+NULL
+listSpecialSets <- function(...){
+  .Deprecated("set6::listSpecialSets", "distr6", "listSpecialSets has been deprecated, use set6::listSpecialSets instead.")
 }
