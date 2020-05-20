cbf913caef65944b88dae1295f8eeef3cef54fa3
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-05-20 11:23:42 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@ R62Fun(DistributionWrapper, assignEnvir = topenv(), dispatchClasses = list(Distr
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\n-----------------------------")
-  packageStartupMessage("\tdistr6 v 1.3.6",
+  packageStartupMessage("\tdistr6 v 1.3.7",
 "\n\nGet started:\t?distr6
 Changelog:\tdistr6News()")
   packageStartupMessage("-----------------------------\n")
