e42171d90f2241adbfdbc037a2ca9fb0bbb60d16
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-06-19 15:34:03 +0800

    Update Pdf2Norm
    test - fail - due to decimal place

--- tests/testthat/test-KernelTricube.R
+++ tests/testthat/test-KernelTricube.R
@@ -4,7 +4,7 @@ test_that("autotest", {
   autotest_kernel(Tricube,
     shortname = "Tric",
     variance = 35 / 243,
-    pdfSquared2Norm = c(175 / 247, 01, 03),
+    pdfSquared2Norm = c(175 / 247, 0.1514387, 0),
     support = Interval$new(-1, 1),
     pdf = c(0.8616, 0.8642, 0.8616)
   )
