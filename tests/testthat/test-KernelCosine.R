8b3864ab55043112c4cacca2e4bb47353851d702
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-06-19 11:47:29 +0800

    update kernel
    test - failure - due to decimal place

--- tests/testthat/test-KernelCosine.R
+++ tests/testthat/test-KernelCosine.R
@@ -4,7 +4,7 @@ test_that("autotest", {
   autotest_kernel(Cosine,
     shortname = "Cos",
     variance = 1 - (8 / (pi^2)),
-    pdfSquared2Norm = c((pi^2) / 16, 01, 03),
+    pdfSquared2Norm = c((pi^2) / 16,  0.1963495, 0),
     support = Interval$new(-1, 1),
     pdf = c(0.7757, 0.7854, 0.7757),
     cdf = c(0.4218, 0.5, 0.5782)
