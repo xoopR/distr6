e110f9d1da180c1bc359d01c283370fa681959b9
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-07-20 10:21:31 +0100

    bugfix: markup error
    Weird error in formatting of [Link](s), removed parantheses
    
     Branch: master

--- man-roxygen/method_wrappedModels.R
+++ man-roxygen/method_wrappedModels.R
@@ -1,4 +1,4 @@
 #' @param model `(character(1))` \cr
-#' id of wrapped [Distribution](s) to return. If `NULL` (default), a list of all wrapped [Distribution]s
-#' is returned; if only one [Distribution] is matched then this is returned, otherwise a list of
-#' [Distribution]s.
+#' id of wrapped [Distribution]s to return. If `NULL` (default), a list of all wrapped
+#' [Distribution]s is returned; if only one [Distribution] is matched then this is returned,
+#' otherwise a list of [Distribution]s.
