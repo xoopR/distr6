f584ffa856af99597df483edb85767df14ca0304
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-07-29 19:22:53 +0100

    kernel cosine test example
     Branch: cpp_kernels

--- tests/testthat/test-KernelCosine.R
+++ tests/testthat/test-KernelCosine.R
@@ -10,3 +10,15 @@ test_that("autotest", {
     cdf = c(0.4218, 0.5, 0.5782)
   )
 })
+
+test_that("pdfsquared2norm upper", {
+  kern <- Cosine$new(decorators = "ExoticStatistics")
+  expect_rounded_equal(kern$pdfPNorm(2, upper = 2)^2, kern$pdfSquared2Norm(upper = 2), 4)
+  expect_rounded_equal(kern$pdfPNorm(2, upper = 0)^2, kern$pdfSquared2Norm(upper = 0), 4)
+  expect_rounded_equal(kern$pdfPNorm(2, upper = -1)^2, kern$pdfSquared2Norm(upper = -1), 4)
+})
+
+test_that("pdfsquared2norm x", {
+  kern <- Cosine$new()
+  expect_rounded_equal(kern$pdfSquared2Norm(x = 2), 2)
+})
