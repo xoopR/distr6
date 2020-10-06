32cbd45adef291691f0ca28acc08db28e33c77b6
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-10-06 19:36:47 +0800

    Update test-KernelCosine.R

--- tests/testthat/test-KernelCosine.R
+++ tests/testthat/test-KernelCosine.R
@@ -24,3 +24,19 @@ test_that("pdfsquared2norm x", {
   expect_rounded_equal(kern$pdfSquared2Norm(x = 0), 0.6168503)
   expect_rounded_equal(kern$pdfSquared2Norm(x = -1.2), 0.1104926)
 })
+
+
+test_that("cdfsquared2norm upper", {
+  kern <- Cosine$new(decorators = "ExoticStatistics")
+  expect_rounded_equal(kern$cdfPNorm(2, upper = 2)^2, kern$cdfSquared2Norm(upper = 2), 4)
+  expect_rounded_equal(kern$cdfPNorm(2, upper = 0)^2, kern$cdfSquared2Norm(upper = 0), 4)
+  expect_rounded_equal(kern$cdfPNorm(2, upper = -1)^2, kern$cdfSquared2Norm(upper = -1), 4)
+})
+
+test_that("cdfsquared2norm x", {
+  kern <- Cosine$new()
+  expect_rounded_equal(kern$cdfSquared2Norm(x = 0.5), 0.01003923)
+  expect_rounded_equal(kern$cdfSquared2Norm(x = -0.5), 0.122963637)
+  expect_rounded_equal(kern$cdfSquared2Norm(x = 2.5), 0.0000000)
+})
+
