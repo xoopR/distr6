12d8f355e8c68ec6e2dd63653e6a82114f6c20fa
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-10-21 15:25:47 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -2,10 +2,15 @@
 do_package_checks()
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+  
+get_stage("before_deploy") %>%
+  add_step(step_setup_push_deploy(branch = "master", orphan = FALSE))
+  
+  get_stage("deploy") %>%
+    add_code_step(rmarkdown::render("README.Rmd")) %>%
+    add_step(step_do_push_deploy(commit_paths = c("README.md")))
+  
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
   do_pkgdown()
-  
-  get_stage("deploy") %>%
-    add_code_step(rmarkdown::render("README.Rmd"))
 }
