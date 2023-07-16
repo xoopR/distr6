1055cc7d0f7ed43d03b278ef0efd1545158790c6
Michal Lauer <michal.lauer.25@gmail.com>
2023-07-16 19:12:28 +0200

    Added test for all distributions

--- tests/testthat/test-dparse.R
+++ tests/testthat/test-dparse.R
@@ -2,3 +2,9 @@ test_that("ShortName, ClassName and Alias are unique across all distributions", 
   d6 <- unlist(listDistributions()[,c("ShortName", "ClassName", "Alias")], use.names = F)
   expect_equal(length(d6), unique(length(d6)))
 })
+
+test_that("Every distribution can be created", {
+  d6 <- unlist(listDistributions()[,c("ShortName", "ClassName", "Alias")], use.names = F)
+  d6 <- paste0(d6, "()")
+  expect_invisible(invisible(sapply(d6, dparse)))
+})
