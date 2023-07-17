8cda250210508fb1b740aafc0ab16bbc5bb37ffe
Michal Lauer <michal.lauer.25@gmail.com>
2023-07-17 09:05:36 +0200

    Updated tests

--- tests/testthat/test-dparse.R
+++ tests/testthat/test-dparse.R
@@ -1,12 +1,29 @@
-test_that("ShortName, ClassName and Alias are unique across all distributions", {
-  d6 <- unlist(listDistributions()[,c("ShortName", "ClassName", "Alias")], use.names = F)
-  expect_equal(length(d6), unique(length(d6)))
+test_that("ShortName, ClassName and Alias are unique between distributions", {
+  d6 <- listDistributions()[,(ids = paste(tolower(ShortName), tolower(ClassName), tolower(Alias), sep = ", "))]
+  d6 <- strsplit(d6, ",")
+  # Unique across one class
+  d6 <- unlist(lapply(d6, function(x) unique(trimws(x))))
+  expect_equal(length(d6), length(unique(d6)))
 })
 
-test_that("Every distribution can be created", {
-  d6 <- unlist(listDistributions()[,c("ShortName", "ClassName", "Alias")], use.names = F)
-  d6 <- paste0(d6, "()")
-  expect_invisible(invisible(sapply(d6, dparse)))
+test_that("Every distribution is created with it's propper S3 class", {
+  # Get calls
+  d6 <- listDistributions()[,(ids = paste(tolower(ShortName), tolower(ClassName), tolower(Alias), sep = ", "))]
+  calls <- strsplit(d6, ",")
+  calls <- unlist(lapply(calls, function(x) trimws(x)))
+  calls <- calls[calls != ""]
+  calls <- paste0(calls, "()")
+  # Get classes
+  counts <- strsplit(d6, ",")
+  counts <- unlist(lapply(counts, length))
+  # Fix for Exponential distribution where Alias = "";
+  counts[16] <- counts[16] - 1
+  counts <- mapply(rep, unlist(listDistributions()[, "ClassName"], use.names = F), each = counts)
+  classes <- unlist(counts, use.names = F)
+  # Evaluate
+  for (i in seq_along(classes)) {
+    expect_s3_class(dparse(calls[i]), classes[i])
+  }
 })
 
 test_that("Invalid distributions are not accepted", {
