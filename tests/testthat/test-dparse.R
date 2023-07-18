2eece841965c314b3c252955e7bae9f8ce766d35
Michal Lauer <michal.lauer.25@gmail.com>
2023-07-18 20:50:29 +0200

    Simplified S3 class test

--- tests/testthat/test-dparse.R
+++ tests/testthat/test-dparse.R
@@ -20,11 +20,6 @@ test_that("Distribution parsing works correctly", {
   expect_s3_class(dparse("C2(df = 2)"), "ChiSquared")
 })
 
-
-
-
-
-
 test_that("ShortName, ClassName and Alias are unique between distributions", {
   d6 <- listDistributions()[,(ids = paste(tolower(ShortName), tolower(ClassName), tolower(Alias), sep = ", "))]
   d6 <- strsplit(d6, ",")
@@ -34,29 +29,17 @@ test_that("ShortName, ClassName and Alias are unique between distributions", {
 })
 
 test_that("Every distribution is created with it's propper S3 class", {
-  # TODO: Smarter way?
   # Get calls
   d6 <- listDistributions()[,(ids = paste(tolower(ShortName), tolower(ClassName), tolower(Alias), sep = ", "))]
   calls <- strsplit(d6, ",")
-  calls <- unlist(lapply(calls, function(x) trimws(x)))
-  calls <- calls[calls != ""]
-  calls <- paste0(calls, "()")
+  calls <- unlist(lapply(calls, function(x) paste0(trimws(x), "()")))
   # Get classes
   counts <- strsplit(d6, ",")
   counts <- unlist(lapply(counts, length))
-  # Fix for Exponential distribution where Alias = "";
-  counts[16] <- counts[16] - 1
   counts <- mapply(rep, unlist(listDistributions()[, "ClassName"], use.names = F), each = counts)
   classes <- unlist(counts, use.names = F)
   # Evaluate
   for (i in seq_along(classes)) {
     expect_s3_class(dparse(calls[i]), classes[i])
   }
-})
-
-test_that("Invalid distributions are not accepted", {
-  expect_error(dparse("Norm"), regex = "Call ")
-  expect_error(dparse("T("))
-  expect_error(dparse("LN)"))
-  expect_error(dparse("Ga2ma"))
 })
