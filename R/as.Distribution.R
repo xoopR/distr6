8364db7e8de9c0e74e8a65da8997c9df74223653
john <bblodfon@gmail.com>
2023-08-23 00:36:51 +0200

    fix warning by adding argument of the generic function

--- R/as.Distribution.R
+++ R/as.Distribution.R
@@ -61,6 +61,6 @@ as.Distribution.matrix <- function(obj, fun, decorators = NULL, vector = FALSE) 
 
 #' @rdname as.Distribution
 #' @export
-as.Distribution.array <- function(obj, fun, decorators = NULL) {
+as.Distribution.array <- function(obj, fun, decorators = NULL, vector = FALSE) {
   dstr("Arrdist", pars = setNames(list(obj), fun), decorators = decorators)
 }
