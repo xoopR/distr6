4583c16722d7b178dd4b4270e8598937a8b7af38
john <bblodfon@gmail.com>
2023-09-07 17:56:35 +0200

    make mixMatrix() compatible with Arrdist inputs

--- tests/testthat/test-mixMatrix.R
+++ tests/testthat/test-mixMatrix.R
@@ -45,3 +45,25 @@ test_that("can mixturise otherwise", {
   m3 <- mixMatrix(list(m1, m2), c(0.1, 0.9))
   expect_equal(m3$cdf(1:5), m1$cdf(1:5) * 0.1 + m2$cdf(1:5) * 0.9)
 })
+
+test_that("works with Arrdist", {
+  m4 <- as.Distribution(
+    t(apply(matrix(runif(4), 2, 2, FALSE,
+      list(NULL, 1:2)), 1,
+      function(x) x / sum(x))),
+    fun = "pdf"
+  )
+
+  pdf5 <- runif(16)
+  arr5 <- array(pdf5, c(2, 2, 4), list(NULL, 1:2, NULL))
+  arr5 <- aperm(apply(arr5, c(1, 3), function(x) x / sum(x)), c(2, 1, 3))
+  darr5 <- Arrdist$new(pdf = arr5, which.curve = "mean")
+
+  pdf6 <- runif(16)
+  arr6 <- array(pdf6, c(2, 2, 4), list(NULL, 1:2, NULL))
+  arr6 <- aperm(apply(arr6, c(1, 3), function(x) x / sum(x)), c(2, 1, 3))
+  darr6 <- Arrdist$new(pdf = arr6, which.curve = 3)
+
+  expect_silent(mixMatrix(list(m4, darr5, darr6)))
+  expect_true(inherits(mixMatrix(list(darr5, darr6)), "Matdist"))
+})
