04f8639d51634968d7385ea43da0c5c47851d800
RaphaelS1 <raphaelsonabend@gmail.com>
2023-10-07 08:21:15 +0100

    fix dimnames

--- R/transformers.R
+++ R/transformers.R
@@ -1,33 +1,37 @@
 pdfcdf <- function(pdf) {
   d <- dim(pdf)
+  dn <- dimnames(pdf)
+
   if (is.null(d)) {
-    cumsum(pdf)
+    return(cumsum(pdf))
   } else if (length(d) == 2) {
-    C_mat_PdfCdf(pdf)
+    out <- C_mat_PdfCdf(pdf)
   } else if (length(d) == 3) {
     # quicker than apply with C_mat_
-    dn <- dimnames(pdf)
     out <- aperm(apply(unname(pdf), c(1, 3), C_vec_PdfCdf), c(2, 1, 3))
-    dimnames(out) <- dn
-    out
   } else {
     stop(sprintf("Expected maximum of three dimensions but got '%s'.", length(d)))
   }
+
+  dimnames(out) <- dn
+  out
 }
 
 cdfpdf <- function(cdf) {
   d <- dim(cdf)
+  dn <- dimnames(cdf)
+
   if (is.null(d)) {
-    c(cdf[1], diff(cdf))
+    return(c(cdf[1], diff(cdf)))
   } else if (length(d) == 2) {
-    C_mat_CdfPdf(cdf)
+    out <- C_mat_CdfPdf(cdf)
   } else if (length(d) == 3) {
     # quicker than apply with C_mat_
-    dn <- dimnames(cdf)
     out <- aperm(apply(unname(cdf), c(1, 3), C_vec_CdfPdf), c(2, 1, 3))
-    dimnames(out) <- dn
-    out
   } else {
     stop(sprintf("Expected maximum of three dimensions but got '%s'.", length(d)))
   }
+
+    dimnames(out) <- dn
+    out
 }
